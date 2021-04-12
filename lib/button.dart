89e73d4ce0d7a4e3a006757185abc3811349b87b
FlutLab.io <>
2021-04-12 20:19:51 +0000

    Initial commit

---
+++ lib/button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class MyButtons extends StatelessWidget {
+
+  final color;
+  final textColor;
+  final String textButton;
+  final buttonTap;
+
+  MyButtons({
+      this.color, this.textButton, this.textColor, this.buttonTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: buttonTap,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Container(
+            color: color,
+            child: Center(
+              child: Text(
+                textButton,
+                style: TextStyle(color: Colors.black, fontSize: 32),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
